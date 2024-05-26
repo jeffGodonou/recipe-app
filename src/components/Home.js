8d0934f75e8a53cbff8d472c27cf2df19848830d
jeff <jeffalecgodonou>
2024-05-26 10:31:53 -0400

    first iteration on styling UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RecipeList from './Recipe';
+import './Home.scss';
 
 const Home = () => {
   return (
@@ -7,8 +8,8 @@ const Home = () => {
       <h1>
         Welcome to Let's Eat !!!
       </h1>
-      <p>
-        Find and share inspiration to cook with what you have in your frige.
+      <p className='description'>
+        Find and share inspiration to cook with what you have in your fridge.
       </p>
       <RecipeList />
     </div>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import './Recipe.scss'
 
 const RecipeList = () => {
     const [recipes, setRecipes] = useState([]);
+    const [searchTerm, setSearchTerm ] = useState('');
 
     useEffect(() => {
         const fecthRecipes = async ()=> {
@@ -10,16 +12,36 @@ const RecipeList = () => {
                 const data = await response.json();
                 setRecipes(data.recipes);
             } catch (error) {
-                console.error('Error met while fetching recipes', error);
+                console.error('Error met while fetching recipes: ', error);
             } 
-            
         }
 
         fecthRecipes();
     }, []);
+
+    const handleSearch = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&apiKey=16f08fcd71a9490db41c6d5827ad4bce`);
+            const data = await response.json();
+            setSearchTerm(data.recipes);
+        } catch (error) {
+            console.error('Error met while searching recipes: ', error);
+        }
+    };
     
     return (
-        <div>
+        <div className="recipe-list-container">
+            <form className="search-form"  onSubmit={handleSearch}>
+                <input 
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Looking for something ?"
+                />
+                <button type="submit">Search</button>
+            </form>
             <h2> Some recipes for you: </h2>
             <ul>
                 {recipes.map(recipe => (
