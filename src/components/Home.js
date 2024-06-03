52760e08b578ee26b8cb2628db3cd2dc86c2707f
jeff <jeffalecgodonou>
2024-06-03 14:39:57 -0400

    Modification of the carousel with a slide

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,39 @@
-import React, { useState } from 'react';
-//import RecipeList from './RecipeList';
+import React, { useEffect, useState } from 'react';
 import RecipeCarousel from './RecipeCarousel';
 import SearchBar from './SearchBar';
 import './Home.scss';
 
 const Home = () => {  
   const [ searchTerm, setSearchTerm ] = useState('');
-  const [, setRecipes] = useState([]); 
+  const [ , setRecipes] = useState([]); 
   const [, setLoading ] = useState(true);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   }; 
 
+  let debounceTimer;
+
   const handleSearch = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`);
-      const data = await response.json();
-      setRecipes(data.meals);
-    } catch (error) {
-      console.log("Error met while fetching the recipes: ", error);
-    } finally {
-      setLoading (false);
-    }
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`);
+        const data = await response.json();
+        setRecipes(data.meals);
+      } catch (error) {
+        console.log("Error met while fetching the recipes: ", error);
+      } finally {
+        setLoading (false);
+      }
+    }, 300);
   };
 
+  useEffect(() => { 
+    handleSearch(); 
+  });
+  
   return (
     <div>
       <h1>
@@ -40,6 +48,9 @@ const Home = () => {
           onChange={handleSearchChange}
           onSearch={handleSearch}
         />
+      </div>
+      <div>
+        
       </div>
       <RecipeCarousel/>
       
--- src/components/RecipeCarousel.js
+++ src/components/RecipeCarousel.js
@@ -1,40 +1,79 @@
 import React, { useEffect, useState } from 'react';
-import Carousel from 'react-material-ui-carousel'; 
+// import Carousel from 'react-material-ui-carousel'; 
+import Slider from 'react-slick';
 import { Card, CardContent, CardMedia, Typography, IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add' ;
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css'
 import { Link } from 'react-router-dom';
 import './RecipeCarousel.scss';
+
+const SampleNextArrow = (props) => {
+    const {className, style, onClick} = props;
+    
+    return (
+        <div
+            className = {className}
+            style = {{ ...style, display: 'block', background: 'black'}}
+            onClick={ onClick }
+        />
+    );
+}
+
+const SamplePrevArrow = (props) => {
+    const {className, style, onClick} = props;
+    
+    return (
+        <div
+            className = {className}
+            style = {{ ...style, display: 'block', background: 'black'}}
+            onClick={ onClick }
+        />
+    );
+}
 
 const RecipeCarousel = () => {
     const [recipes, setRecipes] = useState([]);
 
+    
     useEffect(() => {
         const fetchRecipes = async () => {
-            try {
-                const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-                const data = await response.json();
-                setRecipes(data.meals);
-            } catch (error) {
-                console.error("Error met while fetching the recipes: ", error);
-            }
+
+                    try {
+                        const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+                        const data = await response.json();
+                        setRecipes(data.meals);
+                    } catch (error) {
+                        console.error("Error met while fetching the recipes: ", error);
+                    }
         };
 
         fetchRecipes(); 
-    }, []);
+    }, []);    
 
     if (!recipes || recipes.length === 0) 
         return <div> Loading... </div>; // Show a loading state or a message if there are no recipes
 
+    const settings = {
+        infinite: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+        speed: 500,
+        focusOnSelect: true,
+        nextArrow: < SampleNextArrow />,
+        prevArrow: < SamplePrevArrow />
+    };
+
     return (
         
-        <Carousel showThumbs={false} infiniteLoop useKeyboardArrows
+/*        <Carousel showThumbs={false} infiniteLoop useKeyboardArrows
             navButtonsAlwaysVisible
             indicatorContainerProps={{
             style: {
                 position: 'relative',
                 marginTop: '50px',
-                // height: '90vh',
                 backgroundColor:'white',
             },
             }}
@@ -44,9 +83,13 @@ const RecipeCarousel = () => {
                 color: '#000',
             },
             }}
-        >
+        > */
+        < Slider {...settings}>
+
+        
         {
             recipes.map((recipe) => (
+                <div key={recipe.idMeal}  className='carousel-card'>
                 <Card key={recipe.idMeal} sx={{ maxWidth: 345, margin: 'auto', boxShadow: 3}} className='recipe-card'>
                     <CardMedia
                         component="img"
@@ -63,8 +106,10 @@ const RecipeCarousel = () => {
                         </IconButton>
                     </CardContent>
                 </Card>
+                </div>
             ))}
-        </Carousel>
+        </Slider>
+ //       </Carousel>
         
     );
 };
