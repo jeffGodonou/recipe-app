14f9c0ef3f80b0b69c3be489de29365fa8ca364d
jeff <jeffalecgodonou>
2024-07-04 19:16:00 -0400

    Add of a modification option on personal recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,17 +1,17 @@
-import { Button, Card, CardMedia, CardContent, CircularProgress, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Button, Card, CardMedia, CardContent, CircularProgress, List, ListItem, ListItemText, TextField, Typography } from '@mui/material';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import TextEditor from './TextEditor.js';
 import ShoppingList from './ShoppingList.js';
 import './Recipe.scss';
 
-const Recipe = ({recipes, onAddShoppingList}) => {
+const Recipe = ({recipes, updateRecipe, onAddShoppingList}) => {
     const { id } = useParams();
     const [ fullRecipes, setFullRecipes] = useState(recipes);
     const [ loading, setLoading ] = useState(true);
     const [ notes, setNotes ] = useState('');
-    const [open, setOpen] = useState(false);
-
+    const [ open, setOpen ] = useState(false);
+    
     const handleClickOpen = ()=> { setOpen(true) };
 
     const handleClose = () => { setOpen(false) };
@@ -36,9 +36,27 @@ const Recipe = ({recipes, onAddShoppingList}) => {
     }, [id, recipes, setFullRecipes]);
     
     const recipe = fullRecipes.find(r => r.idMeal === id);
+    const [ isEditing,  setIsEditing ] = useState(false);
+    const [ editableRecipe, setEditableRecipe ] = useState(recipe);
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    }
 
     const handleNotesChange = (event) => {
         setNotes(event.target.value);
+    }
+
+    // capture the changes in editableRecipe
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEditableRecipe(prevState => ({...prevState, [name]: value}));
+    }
+
+    // save the change that had been added 
+    const handleSave = () => {
+        updateRecipe(editableRecipe);
+        setIsEditing(false);
     }
 
     const handleSaveNotes = () => {
@@ -74,51 +92,93 @@ const Recipe = ({recipes, onAddShoppingList}) => {
                     >
                         { recipe.strMeal }
                     </Typography>
-                    <Button
-                        variant="contained"
-                        
-                        onClick={() => handleClickOpen()}    
-                    >
-                        Shopping list
-                    </Button>
-                    <ShoppingList open={open} handleClose={handleClose} onAddShoppingList={onAddShoppingList}/>
+                    <div>
+                        <Button
+                            variant="contained"
+                            onClick={() => handleClickOpen()}    
+                        >
+                            Shopping list
+                        </Button>
+                        {recipe.personal && (
+                            <Button variant="contained" onClick={handleEditClick}>
+                                Edit
+                            </Button>
+                        )}
+                        <ShoppingList open={open} handleClose={handleClose} onAddShoppingList={onAddShoppingList}/>
+                    </div>
                 </div>
-                
                 {recipe.personal && (
                     <Typography variant='body2' color='textSecondary'>
                         Personal Recipe
                     </Typography>
                 )}
-                <Typography variant = "h6" > 
-                    Ingredients
-                </Typography>
-                <ul>
-                    {
-                        Object.keys(recipe).filter(( key ) => key.startsWith('strIngredient') && recipe[key])
-                                            .map(( key ) => (
-                                                <li key = { key } >
-                                                    { recipe[key] }
-                                                </li>))}
-                    {
-                        ingredients.map((ingredient, index) => (
-                            <ListItem key={index}>
-                                <ListItemText primary={ingredient} />
-                            </ListItem>
-                        ))
-                    }
-                </ul>
-                <Typography variant = "h6" > 
-                    Instructions
-                </Typography>
-                <List className="instructions-list">
-                    { 
-                        instructions.map((instruction, index) => ( instruction && (
-                            <ListItem key={index}>
-                                <ListItemText primary={instruction} className='instructions-text'/>
-                            </ListItem>
-                            )
-                    ))}
-                </List>
+
+                {
+                    isEditing ? (
+                        <div>
+                            <TextField
+                                label="Recipe name"
+                                value={editableRecipe.strMeal}
+                                onChange={ handleChange }
+                                fullWidth
+                                margin="normal"
+                                color="success"
+                            />
+                            <TextField
+                                label="Ingredients"
+                                multiline
+                                rows={4}
+                                value={editableRecipe.strIngredients}
+                                onChange={ handleChange }
+                                fullWidth
+                                margin="normal"
+                                color="success"
+                            />
+                            <TextField
+                                label="Instructions"
+                                multiline
+                                rows={4}
+                                value={editableRecipe.strInstructions}
+                                onChange={ handleChange }
+                                fullWidth
+                                margin="normal"
+                                color="success"
+                            />
+                            <Button
+                                variant="contained"
+                                color="success"
+                                onClick={ handleSave }
+                            >      
+                                Save
+                            </Button>
+                        </div>
+                    ) : (
+                        <><Typography variant="h6">
+                                Ingredients
+                            </Typography><ul>
+                                    {!recipe.personal && (Object.keys(recipe).filter((key) => key.startsWith('strIngredient') && recipe[key])
+                                        .map((key) => (
+                                            <li key={key}>
+                                                {recipe[key]}
+                                            </li>))) }
+                                    {ingredients.map((ingredient, index) => (
+                                        <ListItem key={index}>
+                                            <ListItemText primary={ingredient} />
+                                        </ListItem>
+                                    ))}
+                                </ul><Typography variant="h6">
+                                    Instructions
+                                </Typography><List className="instructions-list">
+                                    {instructions.map((instruction, index) => (instruction && (
+                                        <ListItem key={index}>
+                                            <ListItemText primary={instruction} className='instructions-text' />
+                                        </ListItem>
+                                    )
+                                    ))}
+                                </List></>
+                        )
+                }
+
                 <Typography variant="h6">Notes</Typography>
                 <TextEditor
                     label="Your notes"
