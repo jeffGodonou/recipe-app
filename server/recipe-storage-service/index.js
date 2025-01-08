e89f41115f1057ee32cd363d1ecdb683a7574186
jeffGodonou <jeff1949@hotmail.fr>
2025-01-07 19:54:55 -0500

    Add: Setting up Morgan and Winston to ease debugging and generate log server-side

--- server/recipe-storage-service/index.js
+++ server/recipe-storage-service/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const bodyParser = require('body-parser');
+
 const recipesRouter = require('./routes/recipes');
 const shoppingListsRouter = require('./routes/shoppingLists');
 
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json({ limit: '10gb' }));
-// app.use(bodyParser.json());
 
 app.use('/api/recipes', recipesRouter);
 app.use('/api/shopping-lists', shoppingListsRouter);
